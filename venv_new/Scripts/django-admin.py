d5ae682bde9a03a3e87e0c2a3596fa4cba3d37db
Maciej Gyurkowich <maciej.gyurkowich@gmail.com>
2021-01-09 21:34:46 +0100

    v0.0.1
    Terminada la plantilla general

---
+++ venv_new/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maciej\pycharmprojects\interfacesuc3\venv_new\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
