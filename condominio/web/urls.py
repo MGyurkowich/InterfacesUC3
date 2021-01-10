e8ca0f5c48a6a3d7406ab18159ad757efe67e90c
Maciej Gyurkowich <maciej.gyurkowich@gmail.com>
2021-01-10 23:26:12 +0100

    hecho
    Home, perfil, mensajes

--- condominio/web/urls.py
+++ condominio/web/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='web-home')
+    path('', views.home, name='web-home'),
+    path('perfil/', views.profile, name='web-profile'),
+    path('mensajes/', views.messages, name='web-msg')
 ]
--- condominio/web/views.py
+++ condominio/web/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'web/home.html')
+
+
+def profile(request):
+    return render(request, 'web/profile.html')
+
+
+def messages(request):
+    return render(request, 'web/messages.html')
